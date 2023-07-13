a8c7c8165ec8939571815ff9530f619a0aefcff1
Manuel Alanis <manuelaag99@gmail.com>
2023-07-13 12:07:13 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import "./app.scss";
+
+function App() {
+
+  return (
+    <>
+      <div className='big-container '>
+        <div className='top-container'></div>
+        <div className='middle-container'>
+          <div className='middle-left-container'></div>
+          <div className='middle-right-container'></div>
+        </div>
+        <div className='bottom-container'>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
